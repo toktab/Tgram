176c315c50bc70b312bc95be803a9dac19a40f97
toktab <tabagaritoko834@gmail.com>
2024-01-21 10:11:42 -0800

    Initializing

---
+++ src/main/java/dev/toktab/Tgram/TgramApplication.java
@@ -0,0 +1,13 @@
+package dev.toktab.Tgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/toktab/Tgram/TgramApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.toktab.Tgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

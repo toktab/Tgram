d277c2edd27e9ed489c2551a91c9354b62b9a773
toktab <tabagaritoko834@gmail.com>
2024-01-23 04:00:20 -0800

    Model -> User

---
+++ src/main/java/dev/toktab/Tgram/model/User.java
@@ -0,0 +1,34 @@
+package dev.toktab.Tgram.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;//auto
+    @Column(unique = true)
+    private String username;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private LocalDateTime createdOn;//auto
+    private LocalDateTime updatedOn;//auto
+    private boolean isEnabled;//auto
+    private String picture;
+    private String roles;
+
+    //post example
+//    {
+//            "username":"alcatras",
+//            "email":"tabagaritoko834@gmail.com",
+//            "password":"babusprocc1",
+//            "picture":"https://miro.medium.com/v2/resize:fit:1400/1*2eBdh0vLZjUyCDF6x1EqvQ.png",
+//            "roles":"ADMIN"
+//    }
+}
